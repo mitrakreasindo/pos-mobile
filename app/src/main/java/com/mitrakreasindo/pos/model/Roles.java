e9c4a0df9280e61628a62957370c7d66547ad19c
RemnantMaster <maksudihendric@gmail.com>
2017-05-19 16:49:28 +0700

    add model Roles

---
+++ app/src/main/java/com/mitrakreasindo/pos/model/Roles.java
@@ -0,0 +1,62 @@
+package com.mitrakreasindo.pos.model;
+
+/**
+ * Created by hendric on 2017-05-19.
+ */
+
+public class Roles {
+    private String id;
+    private String name;
+    private byte[] permissions;
+    private int rightslevel;
+    private String siteguid;
+    private boolean sflag;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(byte[] permissions) {
+        this.permissions = permissions;
+    }
+
+    public int getRightslevel() {
+        return rightslevel;
+    }
+
+    public void setRightslevel(int rightslevel) {
+        this.rightslevel = rightslevel;
+    }
+
+    public String getSiteguid() {
+        return siteguid;
+    }
+
+    public void setSiteguid(String siteguid) {
+        this.siteguid = siteguid;
+    }
+
+    public boolean isSflag() {
+        return sflag;
+    }
+
+    public void setSflag(boolean sflag) {
+        this.sflag = sflag;
+    }
+}
